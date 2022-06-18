44c9878a5b5de586b83824903e7eaca93e418d2a
최민한 <chmh0411@gmail.com>
2022-06-18 14:30:11 +0900

    add: parentViewController

---
+++ Sources/MinanKit/UIResponder+Extension.swift
@@ -0,0 +1,8 @@
+import Foundation
+import UIKit
+
+extension UIResponder {
+    public var parentViewController: UIViewController? {
+        return next as? UIViewController ?? next?.parentViewController
+    }
+}
